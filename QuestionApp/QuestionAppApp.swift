63bb4c1bc144f1bb3dedf2ffadb5e4584f3871c2
Emma Finkel <emma.finkel@gmail.com>
2023-04-23 19:20:43 -0400

    Initial Commit

---
+++ QuestionApp/QuestionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionAppApp.swift
+//  QuestionApp
+//
+//  Created by scholar on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
